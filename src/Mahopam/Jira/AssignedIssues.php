59fd99e28a3a9a23a62295ebece1f44cf35fb76f
Mahomedalid Pacheco <mahomedalid.pacheco@softtek.com>
2018-01-30 11:08:53 -0800

    Init commit

---
+++ src/Mahopam/Jira/AssignedIssues.php
@@ -0,0 +1,32 @@
+<?php 
+
+namespace Mahomap\Jira;
+
+use \Mahomap\Jira\Connector as JiraConnector;
+
+class AssignedIssues
+{
+   protected $userId;
+   protected $jiraConnector;
+   
+   public function __construct(JiraConnector $jiraConnector, $userId)
+   {
+      $this->jiraConnector = $jiraConnector;
+      $this->userId = $userId;
+   }
+
+   public function getIssues($clear_cache = false)
+   {
+      $file = "cache/" . date("Ymd") . md5($this->userId) . ".issues";
+
+      if (is_readable($file) && !$clear_cache) {
+         return unserialize(file_get_contents($file));
+      }
+
+      $relativeUrl = "search?jql=assignee=" . $this->userId;
+
+      $response = $this->jiraConnector->get($relativeUrl);
+      file_put_contents($file, serialize($response));
+      return $response;
+   }
+}
---
+++ src/Mahopam/Slack/Webhook.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace Mahopam\Slack;
+use \Curl\Curl;
+
+class Webhook
+{
+   protected $hookUrl;
+
+   public function __construct($hookUrl)
+   {
+      $this->hookUrl = $hookUrl;
+   }
+
+   public function postMsg($options)
+   {
+      $curl = new Curl();
+      return $curl->post($this->hookUrl, ["payload" => json_encode($options)]);
+   }
+}
